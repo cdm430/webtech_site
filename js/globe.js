49c4e790f71e04e37241bbc55767168542ef9ba3
Liam White <lw15302@my.bristol.ac.uk>
2016-05-25 13:51:05 +0100

    planet can be started and stopped

--- js/globe.js
+++ js/globe.js
@@ -1,55 +1,60 @@
 "use strict";
 
+var canvas, canvasWidth, canvasHeight, rad, seg, scene, camera, ambLight,
+    direcLight, renderer, geometry, material, sphere, cloudSphere, cloudMaterial,
+    clouds, rendering, spinning = false;
+
 $(document).ready(function (){
-   $('.activate').on("click", function() {
-       setup();
+  setup();
+  $('.activate').on("click", function() {
+     if (spinning === false) {
        spin();
+       changeButton();
+     }
+     else {
+       stop();
+     }
    });
 });
-
-var canvas, canvasWidth, canvasHeight, rad, seg, scene, camera, ambLight, direcLight, renderer,
-    geometry, material, sphere, cloudSphere, cloudMaterial, clouds;
 
 
 function setup() {
   canvas = document.getElementById('endor-canvas');
   canvasWidth = window.innerWidth / 2;
   canvasHeight = window.innerHeight / 2;
-  rad = window.innerWidth / 2700;
+  rad = window.innerWidth / 2600;
   seg = 32;
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(65, canvasWidth/canvasHeight, 0.01, 1000);
+  scene =         new THREE.Scene();
+  camera =        new THREE.PerspectiveCamera(65, canvasWidth/canvasHeight, 0.01, 1000);
+  ambLight =      new THREE.AmbientLight(0x333333);
 
-  ambLight = new THREE.AmbientLight(0x333333);
-  direcLight = new THREE.DirectionalLight(0xffffff, 0.75);
+  direcLight =    new THREE.DirectionalLight(0xffffff, 0.75);
   direcLight.position.set( 3, 5, 3 );
   camera.position.z = 1.5;
 
-  console.log(canvasWidth);
-  console.log(canvasHeight);
-  renderer = new THREE.WebGLRenderer({canvas: canvas, alpha: true});
-
+  renderer =      new THREE.WebGLRenderer({canvas: canvas, alpha: true});
   renderer.setSize(canvasWidth, canvasHeight);
-  document.body.appendChild(renderer.domElement);
-  geometry = new THREE.SphereGeometry(rad, seg, seg);
 
-  material = new THREE.MeshPhongMaterial({
+  document.body.appendChild(renderer.domElement);
+  geometry =      new THREE.SphereGeometry(rad, seg, seg);
+  material =      new THREE.MeshPhongMaterial({
   map: THREE.ImageUtils.loadTexture('../images/colour_surface.jpg'),
   bumpMap: THREE.ImageUtils.loadTexture('../images/height_map.png'),
   bumpScale:0.5,
   });
-  sphere = new THREE.Mesh(geometry, material);
-  cloudSphere = new THREE.SphereGeometry(rad + 0.01, seg, seg);
 
+  sphere =        new THREE.Mesh(geometry, material);
+  cloudSphere =   new THREE.SphereGeometry(rad + 0.01, seg, seg);
   cloudMaterial = new THREE.MeshPhongMaterial({
   map:          THREE.ImageUtils.loadTexture('../images/clouds.png'),
   transparent:  true
   });
-  clouds = new THREE.Mesh(cloudSphere, cloudMaterial);
+
+  clouds =        new THREE.Mesh(cloudSphere, cloudMaterial);
   scene.add(ambLight);
-
-
   scene.add(direcLight);
+
+
   scene.add(sphere);
   scene.add(clouds);
 }
@@ -58,9 +63,10 @@ function setup() {
 
 
 function spin() {
- var render = function(){
-   requestAnimationFrame(render);
-   sphere.rotation.y += 0.0005;
+  spinning = true;
+  var render = function(){
+    rendering = requestAnimationFrame(render);
+    sphere.rotation.y += 0.0005;
 
    clouds.rotation.y -= 0.0003;
    clouds.rotation.x += 0.0003;
@@ -70,4 +76,13 @@ function spin() {
  render();
 
  $('#static-endor').remove();
+}
+
+
+function stop() {
+  spinning = false;
+  cancelAnimationFrame(rendering);
+  changeButton();
+  var division = querySelector('.planet-side');
+  division.innerHTML = '<img src=\"static-planet.png\" />';
 }
