92fd4f0b93ba8d3777def856685a7524101432d5
Liam White <lw15302@my.bristol.ac.uk>
2016-05-25 16:52:05 +0100

    changed text section on mainpage to have static background and scroll

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 "use strict";
 
-if(addEventListener) {
-    addEventListener("load", start);
-}
-else { attachEvent("onload", start); }
+$(document).on("ready", start);
 
 function start() {
-    $(".main-text").niceScroll({cursorcolor:"white", railalign:"left",
+    $(".text-side").niceScroll({cursorcolor:"white", railalign:"left",
         cursoropacitymin:"0.2"});
     $(window).on("resize", setup);
+    $('.text-side').on("scroll", fadePrompt);
 }
 
 
@@ -21,4 +19,36 @@ function changeButton() {
     else {
         $activateButton.html("Animate");
     }
+}
+
+
+function fadePrompt() {
+    var $textSide = $('.text-side');
+    var $scrollPosition = $textSide.scrollTop();
+    var $paragraphPosition = $('p:first').offset();
+    var $topToParagraph = $paragraphPosition - $scrollPosition;
+    var $prompt = $('.scroll-prompt');
+
+    if($scrollPosition < $paragraphPosition) {
+        console.log("p: " + $paragraphPosition);
+        var difference = $paragraphPosition - $scrollPosition;
+        var ratio = difference / $topToParagraph;
+        $prompt.css({
+            opacity: ratio
+        });
+    }
+    console.log($scrollPosition);
+
+
+
+    //
+    //var x = 1;
+    //var ratio = x / $scrollPosition;
+    //var console.log("offset: " + $prompt.offset().top);
+    //if($prompt.offset() < $scrollPosition){
+    //    console.log("flag");
+    //}
+    //$prompt.css({
+    //    opacity: ratio
+    //});
 }
