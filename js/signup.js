4dfb8cbc742c6aede077ba093dc539d30b695f52
Liam White <lw15302@my.bristol.ac.uk>
2016-05-26 12:41:18 +0100

    working on signup request

--- js/signup.js
+++ js/signup.js
@@ -1,24 +1,27 @@
-"use strict"
+"use strict";
 
-$(document).on("ready", start);
-
-function start() {
-    $("#submit").on("click", submitSignUp);
-}
+$(document).ready( function() {
+    $(".info").on('submit', submitSignUp);
+});
 
 
-function submitSignUp() {
-    if(checkInputs) {
+function submitSignUp(e) {
+    if(checkInputs() === true) {
+        e.preventDefault();
         //var fName = $('#fname').val(), lName = $('#lname').val(), uName = $('#uname').val(),
         //    email = $('#email').val(), pass = $('#pass1').val(), gender = $('#gender').val();
         var details = $('.info').serialize();
-        var url = "signup?" + details;
+        var url = "signup.html" + details;
+
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", url, true);
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200){
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 console.log("Completed");
             }
+        }
+        xmlhttp.send();
+        console.log("SUCCESS");
     }
     else {
         console.log("FAIL");
@@ -28,9 +31,15 @@ function submitSignUp() {
 
 
 function checkInputs() {
-    $(':input').each(function() {
-        if($(this).val === "") return false;
+    var passed = true;
+    var inputs = $('.box').not(':input[type=submit]');
+    $(inputs).each(function() {
+        console.log("value: " + $(this).val());
+        if($(this).val() === "") {
+            passed = false;
+            return;
+        }
     });
-    if($('#pass1') !== $('#pass2')) return false;
-    return true;
+    if(!($('#pass1').val() === $('#pass2').val())) {console.log("flag2"); passed = false; };
+    return passed;
 }
