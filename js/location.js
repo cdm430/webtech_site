765c8aaf98be34a20e5017f9c01a4166392915ab
Chris Meehan <cm15320@bristol.ac.uk>
2016-05-28 14:29:34 +0100

    ticket js tidied

--- js/location.js
+++ js/location.js
@@ -1,6 +1,12 @@
 "use strict";
 
+/*
+ * This script deals solely with the google maps api to display the location
+ * on the event page. The code was all found from the google API site
+ */
+
 function initMap() {
+    // Sets the location and places a marker at Redwood Forect, CA
     var myLatlng = {lat: 41.2146192, lng: -124.0084718};
     var map;
     map = new google.maps.Map(document.getElementById('map'), {
@@ -14,12 +20,14 @@ function initMap() {
     title: 'Click to zoom'
     });
 
+    // Pans back to the loction if moved away from it for 3 seconds
     map.addListener('center_changed', function() {
         window.setTimeout(function() {
             map.panTo(marker.getPosition());
         }, 3000);
     });
 
+    // Zooms in on location when the marker is clicked
     marker.addListener('click', function() {
         map.setZoom(15);
         map.setCenter(marker.getPosition());
--- js/tickets.js
+++ js/tickets.js
@@ -1,16 +1,22 @@
 "use strict";
 
+
+/*
+ * This script deals with the client side of the tickets page. This includes
+ * the act of sending purchase requests, and obtaining information about how
+ * many tickets of each type a user has
+ */
+
+// Global variables for the different ticket types
 var numTickets1, numTickets2, numTickets3;
 
 
-if(addEventListener) {
-    addEventListener("load", start);
-}
-else { attachEvent("onload", start); }
+$(document).ready(start);
 
 
 function start(){
 
+    // Set the screen to the top of the page in case refresh halfway down
     $(window).scrollTop(0);
 
 
@@ -18,6 +24,8 @@ function start(){
 
     checkLoggedIn();
 
+    // Listends for scroll events to animate the package headers appearing
+    // and the arrow disappearing
     $(window).on('scroll', function() {
         var $windowHeight = $(window.top).height();
         var $windowPosition = $(window).scrollTop();
@@ -27,15 +35,16 @@ function start(){
         var $bottomToHeader = $firstHeaderPosition - $windowHeight;
 
 
-
+        // Fade in package header and underline when reached
         $('.packageHeading').each(function() {
-            var $ewokPosition = $(this).offset().top;
+            var $packagePosition = $(this).offset().top;
             if($(this).css("opacity") == 1) return;
-            if($windowPosition + (0.8 * $windowHeight) > $ewokPosition) {
+            if($windowPosition + (0.8 * $windowHeight) > $packagePosition) {
                 $.proxy(ticketTypeEntrance, this)();
             }
         })
 
+        // Fade out arrow when scrolling past
         if($windowPosition < $firstHeaderPosition) {
         console.log("flag");
             var difference = $firstHeaderPosition - $windowBottom;
@@ -49,13 +58,19 @@ function start(){
 }
 
 
+/*
+ * This function changes the package information (the buttons) to how they
+ * should look if a user is already signed in e.g no login button
+ */
 function changePackages() {
     var $buttons = $('.buttons');
     $buttons.each(function() {
         $(this).find('p, .sign-in').remove();
     });
 
+    getTicketInfo();
 
+    // Change text of purchase button on click
     $('.purchase').off().on('click', function() {
         $(this).text("Purchase Another");
         var ticketId = $(this).attr('id');
@@ -63,14 +78,18 @@ function changePackages() {
 
     });
 
-    getTicketInfo();
 
+    // Shift the buttons down if logged in, else they appear too high
     $('.purchase, .terms, .purchase-frosted, .terms-frosted, .procured').animate({
         top: '6em'
     }, 200, 'linear');
 }
 
 
+/*
+ * This function is responsible for actually sending the necessary request
+ * for a ticket purchase to be written to the database on the server side
+ */
 function purchaseTicket(ticketId) {
     var url = "purchaseticket?" + ticketId;
     console.log("url going in is " + url);
@@ -85,7 +104,6 @@ function purchaseTicket(ticketId) {
               return;
           }
           else if(responseString === "writtenticket") {
-              console.log("written ticket to database");
               getTicketInfo();
           }
       }
@@ -94,6 +112,11 @@ function purchaseTicket(ticketId) {
 
 }
 
+
+/*
+ * Gets the ticket information (how many of each ticket the user has) and
+ * changes the display under the buttons accordingly.
+ */
 function getTicketInfo() {
     var url = "gettickets";
 
@@ -118,6 +141,11 @@ function getTicketInfo() {
     xmlhttp.send();
 }
 
+
+/*
+ * If they have any tickets of that type, a green tick is displayed with the
+ * number, else nothing is changed.
+ */
 function fillTicketInfo() {
     if(numTickets1 > 0) {
         $('#ewok-text').text("Tickets Purchased: " + numTickets1);
